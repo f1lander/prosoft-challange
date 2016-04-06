36840357ceb125626f67f1c86ff4793cafd94741
f1lander <=>
2016-04-06 12:49:15 -0600

    Final commit, I couldn't complete

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Calendar Profsoft Challange v1.0
 
+*clone this repo
+
+*npm install http-server
+
+*run http-server from root folder
+
 AUTHOR: Edax Uclés 
--- app/directives.js
+++ app/directives.js
@@ -1,4 +1,66 @@
 app.directive('customCalendar', function ($filter) {
+    var startDate;
+    var numberDays;
+    var countryCode;
+    function buildingCalendars(startDate, numberDays, countryCode) {  
+        
+        var year = startDate.getFullYear();
+        var month = startDate.getMonth();
+        var startingDay = startDate.getDate(); //this a date day
+        var weekDay = startDate.getDay();
+                
+        var calDaysLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+        var calMonthsLabels = ['January', 'February','March', 'April','May', 'June', 'July', 'August', 'September','October', 'November', 'December'];
+
+        var calDaysMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        
+        var monthLength = calDaysMonth[month];
+        
+        var monthName = calMonthsLabels[month];
+        
+        if (month == 1) { // February only!
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+                monthLength = 29;
+            }
+        }            
+        
+        var html = '<table class="calendar-table">';
+        html += '<tr><th colspan="7">';
+        html +=  monthName + "&nbsp;" + year;
+        html += '</th></tr>';
+        html += '<tr class="calendar-header">';
+        for (var i = 0; i <= 6; i++ ){
+        html += '<td class="calendar-header-day">';
+        html += calDaysLabels[i];
+        html += '</td>';
+        }
+        html += '</tr><tr>';
+        
+         // fill in the days
+        var day = 1;
+        // this loop is for is weeks (rows)
+        for (var i = 0; i < 9; i++) {
+            // this loop is for weekdays (cells)
+            for (var j = 0; j <= 6; j++) { 
+                html += '<td class="calendar-day">';
+                if (day <= monthLength && (i > 0 || j >= startingDay) && day >= startingDay) {
+                    html += day;
+                    day++;
+                }
+                html += '</td>';
+            }
+            // stop making rows if we've run out of days
+            if (day > monthLength) {
+                break;
+                } else {
+                html += '</tr><tr>';
+            }
+        }
+        html += '</tr></table>';
+        
+        return html;
+    }
     return{
         restrict:'E',
          scope: {
@@ -7,17 +69,13 @@ app.directive('customCalendar', function ($filter) {
             'countryCode' : '=',
          
         },link: function (scope, element, attrs) {
-            var startDate = scope.$eval(attrs.startDate);
-            var numberDays = scope.$eval(attrs.numberDays);
-            var countryCode = scope.countryCode;
+            startDate = scope.$eval(attrs.startDate);
+            numberDays = scope.$eval(attrs.numberDays);
+            countryCode = scope.countryCode;
             console.log('start date: ', $filter('date')(startDate,'M/d/yyyy'), 'number: ', numberDays, ' country code:', countryCode);
-            cal_days_labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-            calMonthsLabels = ['January', 'February','March', 'April','May', 'June', 'July', 'August', 'September','October', 'November', 'December'];
-
-            calDaysMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
            
         },
-        template: '<table class="calendar-table"><tbody><tr><th colspan="7">April&nbsp;2016</th></tr><tr class="calendar-header"><td class="calendar-header-day">Sun</td><td class="calendar-header-day">Mon</td><td class="calendar-header-day">Tue</td><td class="calendar-header-day">Wed</td><td class="calendar-header-day">Thu</td><td class="calendar-header-day">Fri</td><td class="calendar-header-day">Sat</td></tr><tr><td class="calendar-day"></td><td class="calendar-day"></td><td class="calendar-day"></td><td class="calendar-day"></td><td class="calendar-day"></td><td class="calendar-day">1</td><td class="calendar-day">2</td></tr><tr><td class="calendar-day">3</td><td class="calendar-day">4</td><td class="calendar-day">5</td><td class="calendar-day">6</td><td class="calendar-day">7</td><td class="calendar-day">8</td><td class="calendar-day">9</td></tr><tr><td class="calendar-day">10</td><td class="calendar-day">11</td><td class="calendar-day">12</td><td class="calendar-day">13</td><td class="calendar-day">14</td><td class="calendar-day">15</td><td class="calendar-day">16</td></tr><tr><td class="calendar-day">17</td><td class="calendar-day">18</td><td class="calendar-day">19</td><td class="calendar-day">20</td><td class="calendar-day">21</td><td class="calendar-day">22</td><td class="calendar-day">23</td></tr><tr><td class="calendar-day">24</td><td class="calendar-day">25</td><td class="calendar-day">26</td><td class="calendar-day">27</td><td class="calendar-day">28</td><td class="calendar-day">29</td><td class="calendar-day">30</td></tr></tbody></table>'
+        template: buildingCalendars(startDate,numberDays, countryCode)
     }
 })
+
